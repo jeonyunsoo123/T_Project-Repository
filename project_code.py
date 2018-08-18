e49ced597131b123d4498a994bbfb32ae62dbeba
jeonyunsoo123 <jeonyunsoo123@gmail.com>
2018-08-18 09:20:16 +0900

    final commit

--- project_code.py
+++ project_code.py
@@ -13,17 +13,17 @@ Busan Gifted Education Center 2018 (Highschool 1st) Team Project File
 법적 처벌을 받을 수 있습니다.
 '''
 
-# Import
+# Import ss
 import os
 import time
 
-# Open File
+# Open File aa
 file_num = open("list_num.txt" ,'r')
 file_category = open("list_category.txt",'r')
 file_date = open("list_date.txt",'r')
 file_feature = open("list_feature.txt",'r')
 
-#List
+# List ss
 list_num = []
 list_category = []
 list_date = []
@@ -48,7 +48,12 @@ for i in range(0, len(list_category)):
     list_category[i] = list_category[i].strip()
 print(list_category)
 
-#Error Code
+file_num.close()
+file_date.close()
+file_feature.close()
+file_category.close()
+
+# Error Code
 def call_error():
     os.system("cls")
     print("*" * 70)
@@ -66,67 +71,61 @@ def export_to_file():
     file_category = open("list_category.txt",'w')
     file_date = open("list_date.txt",'w')
     file_feature = open("list_feature.txt",'w')
-        file_num.close()
-        file_date.close()
-        file_feature.close()
-        file_category.close()
+
 
     file_num.write("\n".join(list_num))
     file_category.write("\n".join(list_category))
     file_date.write("\n".join(list_date))
     file_feature.write("\n".join(list_feature))
+
+    file_num.close()
+    file_date.close()
+    file_feature.close()
+    file_category.close()
 # <<<<<  Code Start    >>>>>
 
-# 데이터 무결성 검증
+# Data Integrity Check Process
 def data_integrity_check():
-
     if(len(list_num) == len(list_date) == len(list_feature)):
-        file_num.close()
-        file_date.close()
-        file_feature.close()
-        file_category.close()
 
         return 0
     else:
         return 1
 
-
-
-
 if data_integrity_check() == 1:
     print("=" * 70)
-    print("   2018 부산정보영재교육원 고1 정보심화반 팀 프로젝트 | 데이터 무결성 검증 > 실패 : Input File을 확인해주세요")
+    print("2018 부산정보영재교육원 고1 정보심화반 팀 프로젝트 | 데이터 무결성 검증 > 실패")
     print("=" * 70)
     print("\n")
-    print("   프로그램을 실행하는데 필요한 데이터가 손상되었습니다.\n   프로그램이 5초 뒤 종료됩니다.")
+    print("프로그램을 실행하는데 필요한 데이터가 손상되었습니다.\n\n프로그램이 5초 뒤 종료됩니다.")
     time.sleep(5)
     exit()
-else:
+elif data_integrity_check() == 0:
     print("=" * 70)
-    print("   2018 부산정보영재교육원 고1 정보심화반 팀 프로젝트 | 데이터 무결성 검증 > 완료")
+    print("2018 부산정보영재교육원 고1 정보심화반 팀 프로젝트 | 데이터 무결성 검증 > 완료")
     print("=" * 70)
     print("\n")
-    print("   프로그램을 실행하는데 필요한 데이터의 검증에 성공하였습니다.\n   프로그램이 5초 뒤 실행됩니다.")
-    #time.sleep(5)
-    print("hello")
+    print("프로그램을 실행하는데 필요한 데이터의 검증에 성공하였습니다.\n\n프로그램이 5초 뒤 실행됩니다.")
+    time.sleep(5)
+
     # 프로그램 시작
     while True:
         # 메인 매뉴
         os.system("cls")
         print("=" * 70)
-        print("   2018 부산정보영재교육원 고1 정보심화반 팀 프로젝트 | 메인메뉴 >")
+        print("2018 부산정보영재교육원 고1 정보심화반 팀 프로젝트 | 메인메뉴 >")
         print("=" * 70)
         print("\n")
-        print("   부산정보영재교육원 팀 프로젝트 - 분실물 찾기 프로그램\n\n\n   원하시는 메뉴를 선택해 주세요\n   1. 분실물 검색 / 2. 분실물 관리 / 3. 분실물 목록 보기 / 0. 프로그램 종료\n")
+        print("부산정보영재교육원 팀 프로젝트 - 분실물 찾기 프로그램\n\n원하시는 메뉴를 선택해 주세요\n1. 분실물 검색 / 2. 분실물 관리 / 3. 분실물 목록 보기 / 0. 프로그램 종료\n")
         m_menu = input()
         if m_menu == "1":
             # 분실물 검색 메뉴
             os.system("cls")
             print("=" * 70)
-            print("   2018 부산정보영재교육원 고1 정보심화반 팀 프로젝트 | 메인메뉴 > 분실물 검색")
+            print("2018 부산정보영재교육원 고1 정보심화반 팀 프로젝트 | 메인메뉴 > 분실물 검색")
             print("=" * 70)
             print("\n")
-            print("   분실물을 검색하는 메뉴입니다.\n\n   해당하는 분실물의 종류를 알맞게 입력해 주세요.\n   (분실물 입력 종류 : 옷, 가방, 지갑, 학용품, 전자기기, 기타)")
+            print("분실물을 검색하는 메뉴입니다.\n\n해당하는 분실물의 종류를 알맞게 입력해 주세요.\n(분실물 입력 종류 : 옷, 가방, 지갑, 학용품, 전자기기, 기타)")
             search_menu = input()
             menu_index = []
             search_until = len(list_num)
@@ -144,20 +143,19 @@ else:
             # 분실물 관리 메뉴
             os.system("cls")
             print("=" * 70)
-            print("   2018 부산정보영재교육원 고1 정보심화반 팀 프로젝트 | 관리자 권한 필요")
+            print("2018 부산정보영재교육원 고1 정보심화반 팀 프로젝트 | 관리자 권한 필요")
             print("=" * 70)
             print("\n")
-            print("   분실물 관리 메뉴는 관리자 권한이 필요합니다.\n   아래에 관리자 비밀번호를 입력해 주세요 \n password =>")
+            print("분실물 관리 메뉴는 관리자 권한이 필요합니다.\n\n아래에 관리자 비밀번호를 입력해 주세요\n password =>")
             password = input()
             if password == admin_passwd:
                             os.system("cls")
                             print("<<관리자 비밀번호가 일치합니다>>")
-                        #    time.sleep(3)
-                            print("   2018 부산정보영재교육원 고1 정보심화반 팀 프로젝트 | 메인메뉴 > 분실물 관리")
+                            time.sleep(3)
+                            print("2018 부산정보영재교육원 고1 정보심화반 팀 프로젝트 | 메인메뉴 > 분실물 관리")
                             print("=" * 70)
                             print("\n")
-                            print("   ")
-                            print("어느 작업을 수행하시겠습니까? \n     1. 분실물 등록\n     2.분실물 삭제")
+                            print("어느 작업을 수행하시겠습니까?\n1. 분실물 등록 / 2.분실물 삭제")
                             selection = int(input())
                             if selection == 1:
                                 found_no = len(list_num)
@@ -205,21 +203,18 @@ else:
                                 print("\n\n삭제하실 물품의 일렬번호를 입력해주세요 ->")
                                 del_input = int(input())
                                 list_category[del_input] = '삭제된 물품입니다'
-                                list_date[del_input] = '99999999'
+                                list_date[del_input] = '삭제된 물품입니다'
                                 list_feature[del_input] = '삭제된 물품입니다'
                                 os.system("cls")
                                 print("정상 처리되었습니다\n통계 집계를 위해 일렬번호는 사라지지 않습니다.")
                                 enter_delay()
-
-
             else:
                 print("관리자 패스워드가 일치하지 않습니다. 확인 후 다시 시도해주세요\n")
                 enter_delay()
 
-
-
         elif m_menu == "0":
-            os.system("taskkill /IM python.exe")
+            export_to_file()
+            os.system("taskkill /IM python.exe ")
 
         elif m_menu == '3':
             for i in range(0, len(list_num)):
@@ -231,8 +226,15 @@ else:
         else:
             os.system("cls")
             print("=" * 70)
-            print("   2018 부산정보영재교육원 고1 정보심화반 팀 프로젝트 | 오류")
+            print("2018 부산정보영재교육원 고1 정보심화반 팀 프로젝트 | 오류")
             print("=" * 70)
             print("\n")
-            print("   잘못된 메뉴를 입력하셨습니다. 다시 선택해 주세요 (5초 후 메뉴로 돌아갑니다.)")
+            print("잘못된 메뉴를 입력하셨습니다. 다시 선택해 주세요 (5초 후 메뉴로 돌아갑니다.)")
             time.sleep(5)
+else:
+    print("=" * 70)
+    print("2018 부산정보영재교육원 고1 정보심화반 팀 프로젝트 | 데이터 무결성 검증 > 오류")
+    print("=" * 70)
+    print("\n")
+    print("데이터 무결성 검증이 올바르게 동작하지 않았습니다.\n\n프로그램이 5초 뒤 종료됩니다.")
+    time.sleep(5)
